db9bdb2328e2907859063905b3ee9d979c7792ee
daria-z <daria.zakirova@gmail.com>
2018-04-08 22:17:57 +0300

    Переделана функция генерации похожих объетов

--- js/map.js
+++ js/map.js
@@ -13,33 +13,52 @@ var getRandomNumber = function (lengthOfArray) {
   return Math.floor(Math.random() * lengthOfArray);
 }; // выбор случайного числа из массива
 
+var getFeatures = function () {
+  var featuresArray = []; // массив актуальных фич
+  for (var i = 1; i <= getRandomNumber(PLACE_FEATURES.length); i++) { // цикл для генерации массива актуальных фич из массива возможных фич
+    var index = getRandomNumber(PLACE_FEATURES.length); // выбираем индекс случайного элемента массива
+    var placeFeature = PLACE_FEATURES.slice(index, index + 1); // копируем рандомный элемент из массива возможных фич
+    featuresArray.push(placeFeature); // вставляем элемент в массив актуальных фич
+  }
+  return featuresArray;
+};
+
+var mixPhotos = function () {
+  var photosArray = PLACE_PHOTOS.slice(0);
+  photosArray.reverse();
+  return photosArray;
+};
+
 var generateObject = function () { // функция для генерации одного объекта массива предложений
-  var objectResult = {};
-  objectResult.offer.title = AD_TITLES[getRandomNumber(AD_TITLES.length)];
-  objectResult.offer.address = OBJECT_LOCATION_X + ', ' + OBJECT_LOCATION_Y;
-  objectResult.offer.price = Math.floor(Math.random() * 1000 + 1) * 1000;
-  objectResult.offer.type = PLACE_TYPES[getRandomNumber(PLACE_TYPES.length)];
-  objectResult.offer.rooms = Math.floor(Math.random() * 5 + 1);
-  objectResult.offer.guests = Math.floor(Math.random() * 20 + 1);
-  objectResult.offer.checkin = CHECKIN_TIMES[getRandomNumber(CHECKIN_TIMES.length)];
-  objectResult.offer.checkout = CHECKOUT_TIMES[getRandomNumber(CHECKOUT_TIMES.length)];
-  objectResult.offer.features = featuresArray; // массив актуальных фич
-  objectResult.offer.description = '';
-  objectResult.offer.photos = PLACE_PHOTOS.reverse(); // меняем местами порядок фотографий
-  objectResult.location.x = OBJECT_LOCATION_X;
-  objectResult.location.y = OBJECT_LOCATION_Y;
+  var objectResult = {
+    'author': {
+      'avatar': 'img/avatars/user' + '0' + Math.floor(Math.random() * 8 + 1) + '.png'
+    },
+    'offer': {
+      'title': AD_TITLES[getRandomNumber(AD_TITLES.length)],
+      'address': OBJECT_LOCATION_X + ', ' + OBJECT_LOCATION_Y,
+      'price': Math.floor(Math.random() * 1000 + 1) * 1000,
+      'type': PLACE_TYPES[getRandomNumber(PLACE_TYPES.length)],
+      'rooms': Math.floor(Math.random() * 5 + 1),
+      'guests': Math.floor(Math.random() * 20 + 1),
+      'checkin': CHECKIN_TIMES[getRandomNumber(CHECKIN_TIMES.length)],
+      'checkout': CHECKOUT_TIMES[getRandomNumber(CHECKOUT_TIMES.length)],
+      'features': getFeatures(),
+      'description': '',
+      'photos': mixPhotos()
+    },
+    'location': {
+      'x': OBJECT_LOCATION_X,
+      'y': OBJECT_LOCATION_Y
+    }
+  };
+
   return objectResult;
 };
 
-var objects = []; // массив предложений
-var featuresArray = []; // массив актуальных фич
-
-for (var i = 1; i <= getRandomNumber(PLACE_FEATURES.length); i++) { // цикл для генерации массива актуальных фич из массива возможных фич
-  var placeFeature = PLACE_FEATURES.slice(PLACE_FEATURES[getRandomNumber(PLACE_FEATURES.length)], (PLACE_FEATURES[getRandomNumber(PLACE_FEATURES.length)] + 1)); // копируем рандомный элемент из массива возможных фич
-  featuresArray.push(placeFeature); // вставляем элемент в массив актуальных фич
-}
+var similarObjects = []; // массив предложений
 
 for (var j = 0; j <= 8; j++) { // цикл для генерации массива актуальных предложений
   var object = generateObject(); // генирируем актуальный объект
-  objects.push(object); // Записываем актуальный объект в массив
+  similarObjects.push(object); // Записываем актуальный объект в массив
 }
